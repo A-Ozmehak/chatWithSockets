c8b7b78d1662351ff3b5c3dc77a9d145af705f58
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-05-03 14:31:00 +0200

    Add files via upload

---
+++ client/src/main.ts
@@ -0,0 +1,28 @@
+import "./style.css";
+import { io, Socket } from "socket.io-client";
+import { ServerToClientEvents, ClientToServerEvents } from "../../types";
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+
+const messages = document.getElementById("messages");
+const form = document.getElementById("form");
+const input = document.getElementById("input") as HTMLInputElement;
+
+form?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input?.value) {
+    socket.emit("chat message", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("welcome", (message) => {
+  console.log(message);
+});
+
+socket.on("chat message", (message) => {
+  const item = document.createElement("li");
+  item.textContent = message;
+  messages?.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
