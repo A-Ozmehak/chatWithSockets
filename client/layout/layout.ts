24a33e864ecf0a80c2a407932f9b1480615ed833
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-05-05 12:00:04 +0200

    rooms list visible on rooms page

--- client/layout/layout.ts
+++ client/layout/layout.ts
@@ -1,19 +1,19 @@
 import "./layout.css";
 
-// class Layout {
-//   public getNickname;
-// }
 
-function Layout() {
-  const container = document.createElement("div");
-  container.id = "container";
-  let header = document.createElement("header");
-  header.id = "header";
-  let title = document.createElement("h1");
-  title.innerHTML = "Chit & Chat";
 
-  container.append(header, title);
-  document.body.append(container);
+
+export default function layout() {
+  let footer = document.createElement('footer');
+  let creatorNames = document.createElement('p');
+  creatorNames.innerHTML = "Elin Arntén, Jannie Bäckman Kuurne, Ella Larsson, Anna Özmehak"
+  creatorNames.id = "namesFooter";
+
+  let aside = document.createElement('aside');
+  aside.id = "aside";
+
+  footer.append(creatorNames)
+  document.body.append(footer, aside);
 }
 
-// export default Layout;
+
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,127 +1,164 @@
 import "./style.css";
-import { io, Socket } from "socket.io-client";
-import { ServerToClientEvents, ClientToServerEvents } from "../../types";
+import {io, Socket} from "socket.io-client";
+import {ServerToClientEvents, ClientToServerEvents} from "../../types";
+import layout, {aside} from "../layout/layout";
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io({
-  autoConnect: false,
+    autoConnect: false,
 });
 
-let nickname: string;
+let roomList: string[];
+// let nickname: string;
 let joinedRoom: string;
 
 window.addEventListener("load", () => {
-  renderNameInput();
+    renderNameInput();
+    layout();
 });
 
 function renderNameInput() {
-  document.body.innerHTML = "";
+    document.body.innerHTML = "";
 
-  let header = document.createElement("div");
-  header.id = "header";
+    let header = document.createElement("div");
+    header.id = "header";
 
-  let container = document.createElement("div");
-  container.id = "container";
+    let container = document.createElement("div");
+    container.id = "container";
 
-  let inputContent = document.createElement("div");
-  inputContent.id = "inputContent";
+    let inputContent = document.createElement("div");
+    inputContent.id = "inputContent";
 
-  let nickNameInputHeader = document.createElement("h3");
-  nickNameInputHeader.innerHTML = "ENTER YOUR NICKNAME";
+    let nickNameInputHeader = document.createElement("h3");
+    nickNameInputHeader.innerHTML = "ENTER YOUR NICKNAME";
 
-  let nickNameInput = document.createElement("input");
-  nickNameInput.id = "nickNameInput";
+    let nickNameInput = document.createElement("input");
+    nickNameInput.id = "nickNameInput";
 
-  let logInBtn = document.createElement("button");
-  logInBtn.id = "logInButton";
-  logInBtn.innerHTML = "Continue";
+    let logInBtn = document.createElement("button");
+    logInBtn.id = "logInButton";
+    logInBtn.innerHTML = "Continue";
 
-  logInBtn.addEventListener("click", () => {
-    socket.auth = { nickname: nickNameInput.value };
-    socket.connect();
-  });
-  container.append(inputContent);
-  inputContent.append(nickNameInputHeader, nickNameInput, logInBtn);
-  document.body.append(header, container);
+    logInBtn.addEventListener("click", () => {
+        socket.auth = {nickname: nickNameInput.value};
+        socket.connect();
+    });
+    container.append(inputContent);
+    inputContent.append(nickNameInputHeader, nickNameInput, logInBtn);
+    document.body.append(header, container);
 }
 
-function renderRoomInput() {
-  document.body.innerHTML = "";
+export function renderRoomInput() {
+    document.body.innerHTML = "";
 
-  let container = document.createElement("div");
-  let roomInputHeader = document.createElement("h3");
-  roomInputHeader.innerHTML = "Room name";
+    let container = document.createElement("div");
+    let roomInputHeader = document.createElement("h3");
+    roomInputHeader.innerHTML = "Room name";
 
-  let roomInput = document.createElement("input");
+    let roomInput = document.createElement("input");
+    roomInput.id = "roomName";
 
-  let enterBtn = document.createElement("button");
-  enterBtn.innerHTML = "Log in";
-  enterBtn.addEventListener("click", () => {
-    const room = roomInput.value;
-    if (!room.length) {
-      return;
-    }
-    socket.emit("join", room);
-  });
-  container.append(roomInputHeader, roomInput, enterBtn);
-  document.body.append(container);
+
+
+    let enterBtn = document.createElement("button");
+    enterBtn.innerHTML = "Log in";
+    enterBtn.addEventListener("click", () => {
+        const room = roomInput.value;
+
+        let sideBar = document.getElementById("aside") as HTMLElement
+        let listOfRooms = document.createElement('ul');
+        let listElement = document.createElement('li');
+        listElement.id = "listWithRooms"
+        listElement.textContent = roomInput.value;
+        sideBar.append(listOfRooms)
+        listOfRooms.append(listElement)
+
+
+        if (!room.length) {
+            return;
+        }
+        socket.emit("join", room);
+    });
+    container.append(roomInputHeader, roomInput, enterBtn);
+
+    document.body.append(container);
+    layout()
+
 }
 
 function renderMessageForm() {
-  document.body.innerHTML = "";
+    document.body.innerHTML = "";
 
-  let chatList = document.createElement("ul");
-  chatList.id = "messages";
+    let chatList = document.createElement("ul");
+    chatList.id = "messages";
 
-  let chatInput = document.createElement("input");
-  chatInput.autocomplete = "off";
+    let chatInput = document.createElement("input");
+    chatInput.autocomplete = "off";
 
-  let chatForm = document.createElement("form");
-  chatForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (chatInput.value.length) {
-      socket.emit("message", chatInput.value, joinedRoom);
-    } else {
-    }
-  });
-  let sendButton = document.createElement("button");
-  sendButton.innerHTML = "Send";
 
-  chatForm.append(chatInput, sendButton);
-  document.body.append(chatList, chatForm);
+    let chatForm = document.createElement("form");
+    chatForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (chatInput.value.length) {
+            socket.emit("message", chatInput.value, joinedRoom);
+        } else {
+        }
+    });
+    let sendButton = document.createElement("button");
+    sendButton.innerHTML = "Send";
+
+    chatForm.append(chatInput, sendButton);
+    document.body.append(chatList, chatForm);
+    layout()
+
+
+
 }
 
 socket.on("connect_error", (err) => {
-  if (err.message == "Invalid nickname") {
-    console.log("Tryck again");
-  }
+    if (err.message == "Invalid nickname") {
+        alert("Invalid nickname")
+    }
 });
 
 socket.on("_error", (errorMessage) => {
-  console.log(errorMessage);
+    console.log(errorMessage);
 });
 
 socket.on("roomList", (rooms) => {
-  console.log(rooms);
+    console.log(rooms);
+
+    let roomName = document.getElementById('roomName') as HTMLInputElement
+    let sideBar = document.getElementById("aside") as HTMLElement
+    let listOfRooms = document.createElement('ul');
+    let listElement = document.createElement('li');
+    listElement.id = "listWithRooms"
+    roomList = rooms
+
+    sideBar.append(listOfRooms)
+    listOfRooms.append(listElement)
+
+
 });
 
 socket.on("joined", (room) => {
-  console.log("Joined Room", room)
+    alert("you have joined room: " + room)
+    console.log("Joined Room", room)
     joinedRoom = room;
     renderMessageForm();
-  });
+});
 
-  socket.on("connected", (nickname) => {
+socket.on("connected", (nickname) => {
     nickname = nickname;
     renderRoomInput();
-  });
+});
 
-  socket.on("message", (message, from) => {
+socket.on("message", (message, from) => {
     const chatItem = document.createElement("li");
     chatItem.textContent = from.nickname + ": " + message;
 
     const messageList = document.getElementById("messages");
     if (messageList) {
-      messageList.append(chatItem);
+        messageList.append(chatItem);
     }
     window.scrollTo(0, document.body.scrollHeight);
 });
