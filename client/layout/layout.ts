3bfff993c1ad04637643396d4c4994ee6f83e861
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-05-05 14:10:55 +0200

    moved rooms list outside click function

--- client/layout/layout.ts
+++ client/layout/layout.ts
@@ -6,23 +6,7 @@ export default function layout() {
   creatorNames.innerHTML = "Elin Arntén, Jannie Bäckman Kuurne, Ella Larsson, Anna Özmehak"
   creatorNames.id = "namesFooter";
 
-  let aside = document.createElement('aside');
-  aside.id = "aside";
-
   footer.append(creatorNames)
-  document.body.append(footer, aside);
+  document.body.append(footer);
 }
-
-
-// function Layout() {
-//   const container = document.createElement("div");
-//   container.id = "container";
-//   let header = document.createElement("header");
-//   header.id = "header";
-//   let title = document.createElement("h1");
-//   title.innerHTML = "Chit & Chat";
-
-//   container.append(header, title);
-//   document.body.append(container);
-// }
 
