9ffb9bf92d903b4eb4164f947fe244b8aa9bcb22
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-05-05 15:32:37 +0200

    fixed styling

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,12 @@
 import "./style.css";
-import {io, Socket} from "socket.io-client";
-import {ServerToClientEvents, ClientToServerEvents} from "../../types";
+import { io, Socket } from "socket.io-client";
+import { ServerToClientEvents, ClientToServerEvents } from "../../types";
 import layout from "../layout/layout";
 import "./room.css";
-
+import "./chat.css";
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io({
-    autoConnect: false,
+  autoConnect: false,
 });
 
 let roomList: string[];
@@ -14,158 +14,158 @@ let roomList: string[];
 let joinedRoom: string;
 
 window.addEventListener("load", () => {
-    renderNameInput();
-    layout();
+  renderNameInput();
+  layout();
 });
 
 //Get nickname input
 function renderNameInput() {
-    document.body.innerHTML = "";
+  document.body.innerHTML = "";
 
-    let header = document.createElement("div");
-    header.id = "header";
+  let header = document.createElement("div");
+  header.id = "header";
 
-    let container = document.createElement("div");
-    container.id = "container";
+  let container = document.createElement("div");
+  container.id = "container";
 
-    let inputContent = document.createElement("div");
-    inputContent.id = "inputContent";
+  let inputContent = document.createElement("div");
+  inputContent.id = "inputContent";
 
   let nickNameInputHeader = document.createElement("h2");
   nickNameInputHeader.innerHTML = "Enter your nickname";
 
-    let nickNameInput = document.createElement("input");
-    nickNameInput.id = "nickNameInput";
+  let nickNameInput = document.createElement("input");
+  nickNameInput.id = "nickNameInput";
 
-    let logInBtn = document.createElement("button");
-    logInBtn.id = "logInButton";
-    logInBtn.innerHTML = "Continue";
+  let logInBtn = document.createElement("button");
+  logInBtn.id = "logInButton";
+  logInBtn.innerHTML = "Continue";
 
-    logInBtn.addEventListener("click", () => {
-        socket.auth = {nickname: nickNameInput.value};
-        socket.connect();
-    });
-    container.append(inputContent);
-    inputContent.append(nickNameInputHeader, nickNameInput, logInBtn);
-    document.body.append(header, container);
+  logInBtn.addEventListener("click", () => {
+    socket.auth = { nickname: nickNameInput.value };
+    socket.connect();
+  });
+  container.append(inputContent);
+  inputContent.append(nickNameInputHeader, nickNameInput, logInBtn);
+  document.body.append(header, container);
 }
-
 
 export function renderRoomInput() {
-    document.body.innerHTML = "";
+  document.body.innerHTML = "";
 
-    let roomContainer = document.createElement("div");
-    roomContainer.id = "roomContainer";
+  let roomContainer = document.createElement("div");
+  roomContainer.id = "roomContainer";
 
-    let mainContainer = document.createElement("div");
-    mainContainer.id = "mainContainer";
+  let mainContainer = document.createElement("div");
+  mainContainer.id = "mainContainer";
 
-    let sideContainer = document.createElement("div");
-    sideContainer.id = "sideContainer";
+  let sideContainer = document.createElement("div");
+  sideContainer.id = "sideContainer";
 
-    let rheader = document.createElement("div");
-    rheader.id = "rheader";
+  let rheader = document.createElement("div");
+  rheader.id = "rheader";
 
-    let roomInputHeader = document.createElement("h2");
-    roomInputHeader.innerHTML = "Room name";
+  let roomInputHeader = document.createElement("h2");
+  roomInputHeader.innerHTML = "Room name";
 
-    let roomInput = document.createElement("input");
-    roomInput.id = "roomName";
+  let roomInput = document.createElement("input");
+  roomInput.id = "roomName";
 
-    let enterBtn = document.createElement("button");
-    enterBtn.id = "enterBtn";
-    enterBtn.innerHTML = "Log in";
+  let enterBtn = document.createElement("button");
+  enterBtn.id = "enterBtn";
+  enterBtn.innerHTML = "Log in";
 
-    let listOfRooms = document.createElement('ul');
-    listOfRooms.id = "listRooms"
-    let listElement = document.createElement('li');
-    listElement.id = "listWithRooms"
-    sideContainer.append(listOfRooms)
-    listOfRooms.append(listElement)
+  let listOfRooms = document.createElement("ul");
+  listOfRooms.id = "listRooms";
+  let listElement = document.createElement("li");
+  listElement.id = "listWithRooms";
+  sideContainer.append(listOfRooms);
+  listOfRooms.append(listElement);
 
-    enterBtn.addEventListener("click", () => {
-        const room = roomInput.value;
+  enterBtn.addEventListener("click", () => {
+    const room = roomInput.value;
 
-        listElement.textContent = roomInput.value;
+    listElement.textContent = roomInput.value;
 
-        if (!room.length) {
-            return;
-        }
-        socket.emit("join", room);
-    });
+    if (!room.length) {
+      return;
+    }
+    socket.emit("join", room);
+  });
 
-    sideContainer.append(roomContainer);
-    mainContainer.append(sideContainer, rheader);
-    roomContainer.append(roomInputHeader, roomInput, enterBtn);
-    document.body.append(mainContainer);
-    layout()
+  sideContainer.append(roomContainer);
+  mainContainer.append(sideContainer, rheader);
+  roomContainer.append(roomInputHeader, roomInput, enterBtn);
+  document.body.append(mainContainer);
+  layout();
 }
 
-    function renderMessageForm() {
-        document.body.innerHTML = "";
+function renderMessageForm() {
+  document.body.innerHTML = "";
 
-        let chatList = document.createElement("ul");
-        chatList.id = "messages";
+  let chatList = document.createElement("ul");
+  chatList.id = "messages";
 
-        let chatInput = document.createElement("input");
-        chatInput.autocomplete = "off";
+  let chatInput = document.createElement("input");
+  chatInput.id = "chatInput";
+  chatInput.autocomplete = "off";
 
-        let chatForm = document.createElement("form");
-        chatForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            if (chatInput.value.length) {
-                socket.emit("message", chatInput.value, joinedRoom);
-                chatForm.reset();
-            } else {
-            }
-        });
-        let sendButton = document.createElement("button");
-        sendButton.innerHTML = "Send";
-
-        chatForm.append(chatInput, sendButton);
-        document.body.append(chatList, chatForm);
-        layout()
+  let chatForm = document.createElement("form");
+  chatForm.id = "chatForm";
+  chatForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (chatInput.value.length) {
+      socket.emit("message", chatInput.value, joinedRoom);
+      chatForm.reset();
+    } else {
     }
+  });
+  let sendButton = document.createElement("button");
+  sendButton.id = "sendButton";
+  sendButton.innerHTML = "Send";
 
-    socket.on("connect_error", (err) => {
-        if (err.message == "Invalid nickname") {
-            alert("Invalid nickname")
-        }
-    });
+  chatForm.append(chatInput, sendButton);
+  document.body.append(chatList, chatForm);
+  layout();
+}
 
-    socket.on("_error", (errorMessage) => {
-        console.log(errorMessage);
-    });
+socket.on("connect_error", (err) => {
+  if (err.message == "Invalid nickname") {
+    alert("Invalid nickname");
+  }
+});
 
-    socket.on("roomList", (rooms) => {
-        console.log(rooms);
-    });
+socket.on("_error", (errorMessage) => {
+  console.log(errorMessage);
+});
 
-    socket.on("joined", (room) => {
-        alert("you have joined room: " + room)
-        console.log("Joined Room", room)
-        joinedRoom = room;
-        renderMessageForm();
-    });
+socket.on("roomList", (rooms) => {
+  console.log(rooms);
+});
 
+socket.on("joined", (room) => {
+  alert("you have joined room: " + room);
+  console.log("Joined Room", room);
+  joinedRoom = room;
+  renderMessageForm();
+});
 
-    socket.on("message", (message, from) => {
-        const chatItem = document.createElement("li");
-        chatItem.textContent = from.nickname + ": " + message;
+socket.on("message", (message, from) => {
+  const chatItem = document.createElement("li");
+  chatItem.id = "chatItem";
+  chatItem.textContent = from.nickname + ": " + message;
 
-        const messageList = document.getElementById("messages");
-        if (messageList) {
-            messageList.append(chatItem);
-        }
-        window.scrollTo(0, document.body.scrollHeight);
+  const messageList = document.getElementById("messages");
+  if (messageList) {
+    messageList.append(chatItem);
+  }
+  window.scrollTo(0, document.body.scrollHeight);
 
+  //joinedRoom = room;
+  //renderMessageForm();
+});
 
-        //joinedRoom = room;
-        //renderMessageForm();
-    });
-
-    socket.on("connected", (nickname) => {
-        nickname = nickname;
-        renderRoomInput();
-    });
-
+socket.on("connected", (nickname) => {
+  nickname = nickname;
+  renderRoomInput();
+});
